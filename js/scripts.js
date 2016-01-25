f64d2b0d7d20f2ca3fd88fb686c1c95a6dd9e8e2
Jessica Fix <jcfix.pdx@gmail.com>
2016-01-25 09:27:26 -0800

    Add margins, show-contact div, some non-functional jquery

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,32 @@ function Contact(firstName, lastName) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+
+
+
+
+// User Logic //
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var userFirstName = $("input#new-first-name").val();
+    var userLastName = $("input#new-last-name").val();
+    var newContact = new Contact(userFirstName, userLastname);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+    $(".contact").last().click(function(){
+      $("#show-contact").show();
+
+
+
+    });
+
+
+  });
+});
